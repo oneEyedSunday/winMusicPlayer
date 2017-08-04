cff9eb757cc169698d14eea2dae972c2095243bd
Idiakose Osemudiame Sunday <idiakosesunday@gmail.com>
2017-08-04 21:31:40 +0100

    Initial Commit, finally got around to fix the progress bar after a month away from it. To provide seek functionality on progressbar and album art from folders.

---
+++ ISPOAmusic/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ISPOAmusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mute_btn = new System.Windows.Forms.Button();
+            this.fw_btn = new System.Windows.Forms.Button();
+            this.play_btn = new System.Windows.Forms.Button();
+            this.rew_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 198);
+            this.panel1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 154);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 15);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.mute_btn);
+            this.panel2.Controls.Add(this.fw_btn);
+            this.panel2.Controls.Add(this.play_btn);
+            this.panel2.Controls.Add(this.rew_btn);
+            this.panel2.Controls.Add(this.stop_btn);
+            this.panel2.Location = new System.Drawing.Point(12, 217);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 32);
+            this.panel2.TabIndex = 1;
+            // 
+            // mute_btn
+            // 
+            this.mute_btn.Location = new System.Drawing.Point(216, 3);
+            this.mute_btn.Name = "mute_btn";
+            this.mute_btn.Size = new System.Drawing.Size(41, 23);
+            this.mute_btn.TabIndex = 4;
+            this.mute_btn.Text = "Mute";
+            this.mute_btn.UseVisualStyleBackColor = true;
+            this.mute_btn.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // fw_btn
+            // 
+            this.fw_btn.Location = new System.Drawing.Point(170, 6);
+            this.fw_btn.Name = "fw_btn";
+            this.fw_btn.Size = new System.Drawing.Size(39, 23);
+            this.fw_btn.TabIndex = 3;
+            this.fw_btn.Text = "FW";
+            this.fw_btn.UseVisualStyleBackColor = true;
+            this.fw_btn.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // play_btn
+            // 
+            this.play_btn.Location = new System.Drawing.Point(91, 6);
+            this.play_btn.Name = "play_btn";
+            this.play_btn.Size = new System.Drawing.Size(75, 23);
+            this.play_btn.TabIndex = 2;
+            this.play_btn.Text = "Play";
+            this.play_btn.UseVisualStyleBackColor = true;
+            this.play_btn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // rew_btn
+            // 
+            this.rew_btn.Location = new System.Drawing.Point(41, 6);
+            this.rew_btn.Name = "rew_btn";
+            this.rew_btn.Size = new System.Drawing.Size(44, 23);
+            this.rew_btn.TabIndex = 1;
+            this.rew_btn.Text = "RW";
+            this.rew_btn.UseVisualStyleBackColor = true;
+            this.rew_btn.Click += new System.EventHandler(this.rew_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(3, 4);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(32, 23);
+            this.stop_btn.TabIndex = 0;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(194, 272);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Open";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 318);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button mute_btn;
+        private System.Windows.Forms.Button fw_btn;
+        private System.Windows.Forms.Button play_btn;
+        private System.Windows.Forms.Button rew_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ISPOAmusic/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ISPOAmusic
+{
+    public partial class Form1 : Form
+    {
+        bool inPlay = false;
+        bool isStopped = false;
+        double curpos = 0;
+        
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+
+        bool playpause(bool test)
+        {
+            if (test == true)
+            {
+                player.controls.pause();
+                play_btn.Text = "Play";
+                curpos = player.controls.currentPosition;
+                label1.Text = "Paused";
+                return false;
+            }
+/*
+            else if ((test == true) && (isStopped == true))
+            {
+                player.URL = textBox1.Text;
+                player.controls.play();
+                play_btn.Text = "Pause";
+                label1.Text = "In Play";
+                test = true;
+                return test;
+            }
+*/
+            else
+            {
+                player.URL = textBox1.Text;
+                player.controls.play();
+                label2.Text = player.controls.currentPosition.ToString();
+                player.controls.currentPosition = curpos;
+                play_btn.Text = "Pause";
+                label1.Text = "In Play" + progressBar1.Maximum;
+                test = true;
+                
+                return true;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Not Started";
+            inPlay = false;
+            stop_btn.Enabled = false;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fw_btn.Enabled = false;
+            rew_btn.Enabled = false;
+            mute_btn.Enabled = false;
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "MP3 files (*.mp3)|*.mp3|All Files (*.*)|*.*";
+            ofd.ShowDialog();
+            textBox1.Text = ofd.FileName;
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "") {
+                    MessageBox.Show("Error, Choose a file first");
+                    return;
+                }
+                inPlay = playpause(inPlay);
+                stop_btn.Enabled = true;
+                timer1.Start();
+                fw_btn.Enabled = true;
+                rew_btn.Enabled = true;
+                mute_btn.Enabled = true;
+                //if (player != null)
+                //{
+                //    progressBar1.Maximum = (int)(player.controls.currentItem.duration * 60);
+                //    progressBar1.Minimum = 0;
+                //}
+                
+               
+                
+            }
+            catch(Win32Exception exp){
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            player.controls.stop();
+            label1.Text = "Stopped";
+            //make cur pos 0
+            player.controls.currentPosition = 0;
+            stop_btn.Enabled = false;
+            isStopped = true;
+            play_btn.Text = "Play";
+            inPlay = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (player!=null) 
+            { label2.Text = player.controls.currentPositionString + "/" + player.controls.currentItem.durationString;
+            //check for picture in foler
+            //and make pictureBox1.source = image
+            
+            label3.Text = handleProgress().ToString();
+            progressBar1.Value = handleProgress();
+            if (progressBar1.Value == 100) { progressBar1.Value = 0; }
+            //code to stop and display stopped
+            //implement with delegates and eventhandlers
+            //implement functionality on progress bar
+            //album art
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            player.controls.fastForward();
+            double fwdpos = player.controls.currentPosition + 5;
+            player.controls.play();
+            player.controls.currentPosition = fwdpos;
+        }
+
+        private void rew_btn_Click(object sender, EventArgs e)
+        {
+            player.controls.fastReverse();
+            double rewpos = player.controls.currentPosition - 5;
+            player.controls.play();
+            player.controls.currentPosition = rewpos;
+        }
+
+        private int handleProgress(){
+	        int percent = (int)((player.controls.currentPosition / player.controls.currentItem.duration) * 100);
+            return percent;
+        }
+
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+   
+}
